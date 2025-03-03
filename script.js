3caaee6519316e5f16f85ce0ff92194626b2dd91
Natalie Anderson <natalieg.anderson@mail.utoronto.ca>
2025-03-02 22:52:28 -0500

    added popup legend and carousel

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kZXI5MjQiLCJhIjoiY201b2RweHNhMGxjazJscTI0c
 
 //creating map
 const createMap = (zoomValue) => {
+    const carousel = document.getElementById("carousel")
+    carousel.style.zIndex = "-100"
 	const map = new mapboxgl.Map({
 		container: 'map', 
 		style: 'mapbox://styles/ander924/cm6sdtrs2004t01qshcd17pgy', // style URL
 		center: [-123.126, 49.249], // starting view [lng, lat]
 		zoom: zoomValue, // starting zoom
 		minZoom: zoomValue //can't zoom out
-	});
+	})
 	
 	map.on('load', () => {
 		// Load Public Art GeoJSON into github
@@ -24,9 +26,9 @@ const createMap = (zoomValue) => {
 			source: 'public-art', //using the source for public art added above
 			paint: {
 				'circle-radius': 3, //circle radius size
-				'circle-color': '#B33951',  //colour of circle
+				'circle-color': 'orange',  //colour of circle
 				'circle-stroke-width': 0.5, //border of circle width
-				'circle-stroke-color': '#691E06' //border of circle colour
+				'circle-stroke-color': 'dark orange' //border of circle colour
 			}
 		});
 	
@@ -42,9 +44,9 @@ const createMap = (zoomValue) => {
 			source: 'community-gardens-and-food-trees', //using the source for community gardens and foodtrees added above
 			paint: {
 				'circle-radius': 3, //circle radius size
-				'circle-color': '#284B63', //colour of circle
+				'circle-color': 'blue', //colour of circle
 				'circle-stroke-width': 1, //border of circle width
-				'circle-stroke-color': '#2F3E46' //border of circle colour
+				'circle-stroke-color': 'dark blue' //border of circle colour
 			}
 		})
 	});
@@ -56,20 +58,31 @@ createMap(11)
 // making button change from show legend to hide legend depending if opened or not
 const renameButton = () => {
 	const button = document.getElementById("button");
-	console.log(button.innerHTML)
-	button.innerHTML === "Hide Legend" ? button.innerHTML = "Show Legend" : button.innerHTML = "Hide Legend"
+    const legend = document.getElementById("legend")
+    const carousel = document.getElementById("carousel")
+
+    if (button.innerHTML === "Hide Legend") {
+        button.innerHTML = "Show Legend"
+        legend.style.zIndex = "-100"
+        carousel.style.zIndex = "-100"
+
+    } else {
+        button.innerHTML = "Hide Legend"
+        legend.style.zIndex = "100"
+        carousel.style.zIndex = "100"
+    }
 }
 
-document.getElementById('button').addEventListener('click', () => {
-	const mapDiv = document.getElementById('map');
-//if map has this view than create map11, otherwise create map10.5
-	 if (mapDiv.style.width === '70vw') {//map view width 70%
-		mapDiv.style.width='100vw' //map view width 100%
-		mapDiv.style.height='100vh' //map view height 100%
-		createMap(11)
-	} else {
-		mapDiv.style.width='70vw' //map view width 70%
-		mapDiv.style.height='100vh' //map view height 100%
-		createMap(10.5)
-	}
-})
+// document.getElementById('button').addEventListener('click', () => {
+// 	const mapDiv = document.getElementById('map');
+// //if map has this view than create map11, otherwise create map10.5
+// 	 if (mapDiv.style.width === '70vw') {//map view width 70%
+// 		mapDiv.style.width='100vw' //map view width 100%
+// 		mapDiv.style.height='100vh' //map view height 100%
+// 		createMap(11)
+// 	} else {
+// 		mapDiv.style.width='70vw' //map view width 70%
+// 		mapDiv.style.height='100vh' //map view height 100%
+// 		createMap(10.5)
+// 	}
+// })
